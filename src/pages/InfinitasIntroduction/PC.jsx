534fb9775dbdedbb7d29eaafb0aa96b2f9c5abb3
evercurse <17601547148@163.com>
2023-05-16 15:16:05 +0800

    初始化

---
+++ src/pages/InfinitasIntroduction/PC.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './index.css'
+
+export default function InfinitasIntroductionPC(){
+    return (
+        <div>
+
+        </div>
+    )
+}
--- src/pages/InfinitasIntroduction/index.js
+++ src/pages/InfinitasIntroduction/index.js
@@ -1,8 +1,33 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import './index.css'
+import InfinitasIntroductionMobile from "./Mobile";
+import InfinitasIntroductionPC from "./PC";
+import BigNumber from "bignumber.js";
 
 export default function InfinitasIntroduction(){
+    const [isMobileView,setIsMobileView]=useState(false);//是否是手机视图
+
+    const resizeUpdate = (e) => {
+        // 通过事件对象获取浏览器窗口的高度
+        let width = e.target.innerWidth;
+        if(BigNumber(width).isLessThan(905)){
+            setIsMobileView(true)
+        }else {
+            setIsMobileView(false)
+        }
+
+    };
+
+
+    useEffect(()=>{
+        const width = window.innerWidth;
+        setIsMobileView(BigNumber(width).isLessThan(905))
+        window.addEventListener('resize', resizeUpdate);
+        return () => {
+            window.removeEventListener('resize', resizeUpdate);
+        }
+    },[])
     return (
-        <div>112</div>
+        isMobileView?<InfinitasIntroductionMobile />:<InfinitasIntroductionPC />
     )
 }
