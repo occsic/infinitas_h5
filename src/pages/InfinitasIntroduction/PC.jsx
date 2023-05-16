85f8da90d4b781f2b728901093b565848cae9b7b
evercurse <17601547148@163.com>
2023-05-16 18:33:55 +0800

    1

--- src/pages/InfinitasIntroduction/PC.jsx
+++ src/pages/InfinitasIntroduction/PC.jsx
@@ -4,6 +4,8 @@ import { Carousel } from 'antd';
 import bg1 from './image/pc_bg1.svg'
 import bg2 from './image/pc_bg2.svg'
 import logo from './image/logo.svg'
+import more from  './image/learn_more.svg'
+import floor from './image/floor.svg'
 export default function InfinitasIntroductionPC(){
     return (
         <div className='flex-column'>
@@ -42,10 +44,44 @@ export default function InfinitasIntroductionPC(){
                 </div>
             </Carousel>
 
-            <div className='ptwo'>
+            {/*第二部分*/}
+            <div className='ptwo flex-column fl-ai-center'>
+                <div className='flex-row fl-ai-center fl-jc-between' style={{width:'80%'}}>
+                    <div className='flex-column' style={{flex:0.5}}>
+                        <div className='pfontC' style={{fontSize:'64px'}}>How to understand</div>
+                        <div className='pfontC' style={{fontSize:'142px'}}>RGB</div>
+                        <div className='pfontB' style={{maxWidth:'555px'}}>On top of Bitcoin, integrated lightning network, with unlimited scalability of the smart contract protocol</div>
+                    </div>
+                        <div style={{flex:0.5}} className='flex-row fl-jc-center'>
+                            <img src={more} />
+                        </div>
+                </div>
+                <div className='flex-row fl-ai-center fl-jc-between' style={{width:'80%'}}>
+                    <div  style={{flex:0.5}}>
+                        <img src={floor}/>
+                    </div>
 
+                    <div className='flex-column' style={{flex:0.5}}>
+                        <div className='flex-row fl-ai-center'>
+                            <div className='pcircle'/>
+                            <div className='pfontC' style={{fontSize:'24px',marginLeft:'10px'}}>RGB protocol network (Ecological layer)</div>
+                        </div>
+                        <div className='flex-row fl-ai-center' style={{marginTop:'100px'}}>
+                            <div className='pcircle'/>
+                            <div className='pfontC ' style={{fontSize:'24px',marginLeft:'10px'}}>Lightning Network (Payment Channel)</div>
+                        </div>
+                        <div className='flex-row fl-ai-center'  style={{marginTop:'100px'}}>
+                            <div className='pcircle'/>
+                            <div className='pfontC' style={{fontSize:'24px',margi1nLeft:'10px'}}>BTC blockchain (Security Commitment Layer)</div>
+                        </div>
+                    </div>
+                </div>
             </div>
 
+            {/*第三部分*/}
+            <div className='pthree flex-column fl-ai-center' >
+                <div className='pfontD' style={{marginTop:'20px'}}>What’s new at  <span className='pfontF' style={{fontSize:'120px'}}>Infinitas</span></div>
+            </div>
         </div>
     )
 }
