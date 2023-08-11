f8a9aa06f10b53d17c50d1fa074feca545afa09d
evercurse <17601547148@163.com>
2023-08-11 11:16:46 +0800

    1

--- src/component/PCHeader/index.jsx
+++ src/component/PCHeader/index.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import './index.css'
 import logo from '../../static/Logo.png'
 import {Tooltip,Popover} from "antd";
+import {Link} from "react-router-dom";
 
-export default function PCHeader(){
-    const content = (
+export default function PCHeader(props){
+    const {}=props
+    const content1 = (
         <div className='pch-content'>
             <div className='pchc-text'>ABOUT US</div>
             <div className='pchc-text'>WE STAND FOR</div>
@@ -15,6 +17,15 @@ export default function PCHeader(){
             <div style={{height:'10px'}}/>
         </div>
     );
+    const content2 = (
+        <div className='pch-content'>
+            <div className='pchc-text'>RGB</div>
+            <div className='pchc-text'>LIGHTING NETWORK</div>
+            <div className='pchc-text'>ZK-SNARK</div>
+            <div style={{height:'10px'}}/>
+        </div>
+    );
+
     return (
         <div className='flex-center pc-header-container'>
             <div className='pc-header'>
@@ -22,11 +33,15 @@ export default function PCHeader(){
                     <img src={logo} width={180} height={35}/>
                 </div>
                 <div className='flex-row fl-ai-center fl-jc-between' style={{flex:0.58}}>
-                    <Popover overlayInnerStyle={{padding:'0'}} placement="bottomLeft" color={'rgba(0, 0, 0, 0.2)'}  content={content}>
+                    <Popover overlayInnerStyle={{padding:'0'}} placement="bottomLeft" color={'rgba(0, 0, 0, 0.2)'}  content={content1}>
                         <div className='pch-tag'>HOME</div>
                     </Popover>
 
-                    <div className='pch-tag'>LEARN</div>
+                    <Popover overlayInnerStyle={{padding:'0'}} placement="bottomLeft" color={'rgba(0, 0, 0, 0.2)'}  content={content2}>
+                        <div className='pch-tag'>LEARN</div>
+                    </Popover>
+
+
                     <div className='pch-tag'>RESOURCES</div>
                     <div className='pch-tag'>BUILD</div>
                     <div className='pch-tag'>CONTACT</div>
