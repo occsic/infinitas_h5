9a999f0ba64eeac1c3d8fbc907f44d024bf4f359
alt <>
2023-08-11 15:29:36 +0800

    show

--- src/pages/ZkSnark/Mobile.js
+++ src/pages/ZkSnark/Mobile.js
@@ -6,13 +6,69 @@ import about from "../../static/about.png"
 
 export default function ZkSnarkMobile() {
     const [showIndex, setShowIndex] = useState(0)
+    const SNARK = [
+        {
+            img: about,
+            sub: "The key generator G takes a secret parameter lambda and a program C, and generates two publicly available keys, a proving key pk, and a verification key vk. These keys are public parameters that only need to be generated once for a given program C.",
+        },
+        {
+            img: about,
+            sub: "The prover P takes as input the proving key pk, a public input x and a private witness w. The algorithm generates a proof prf = P(pk, x, w) that the prover knows a witness w and that the witness satisfies the program.",
+        },
+        {
+            img: about,
+            sub: "The verifier V computes V(vk, x, prf) which returns true if the proof is correct, and false otherwise. Thus this function returns true if the prover knows a witness w satisfying C(x,w) == true.",
+        },
+        {
+            img: about,
+            sub: "Note here the secret parameter lambda used in the generator. This parameter sometimes makes it tricky to use zk-SNARKs in real-world applications. The reason for this is that anyone who knows this parameter can generate fake proofs. Specifically, given any program C and public input x a person who knows lambda can generate a proof fake_prf such that V(vk, x, fake_prf) evaluates to true without knowledge of the secret w.",
+        },
+        {
+            img: about,
+            sub: "Thus actually running the generator requires a very secure process to make sure no-one learns about and saves the parameter anywhere. This was the reason for the extremely elaborate ceremony the Zcash team conducted in order to generate the proving key and verification key, while making sure the “toxic waste” parameter lambda was destroyed in the process.",
+        },
 
-
-
+    ]
     return (
         <div>
             <MHeader activeIndex={"2"} />
+            <div className="zk-header content">
+                <div className="flex-row flex-jc-between">
+                    <div className="zk-text colorB">WHAT IS ZK-SNARK?</div>
+                    <img className="zk-header-img" src={about} />
+                </div>
+                <div className="zk-header-line"></div>
+                <div className="zk-sub-title">
+                    Zero-Knowledge Proofs (ZKPs) are a cryptographic concept that allows one entity to prove to another that an assertion is true without revealing any information about the content of
+                    the assertion. Recursive zero-knowledge proof is a further extension and optimization of zero-knowledge proof, which is used to realize the lightweight and decentralization of
+                    blockchain. The basic idea is to compress the state and transaction history of the entire blockchain into a fixed-size proof, called a "Snark", and then embed this proof into each
+                    new block.
+                </div>
+            </div>
+            <div className="zk-main content">
+                <div className="zk-text colorF">A ZK-SNARK CONSISTS OF THREE ALGORITHMS G, P, V DEFINED AS FOLLOWS</div>
+                <div className="zkmainItem">
+                    <div className="flex-row z">
+                        <div>
+                            <img className="zkmainItem-img" src={SNARK[showIndex].img} />
+                            <div className="zkmainItem-sub">{SNARK[showIndex].sub}</div>
+                        </div>
+                        <div className="zkmainItem-ver flex-column">
+                            {SNARK.map((i, ind) => (
+                                <div
+                                    onClick={() => {
+                                        setShowIndex(ind)
+                                    }}
+                                    className={`ver ${showIndex == ind ? "red" : ""}`}
+                                    key={ind}
+                                ></div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            </div>
         
+
             <MFooter activeIndex={"2"} />
         </div>
     )
