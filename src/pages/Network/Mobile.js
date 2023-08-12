50f06e7e8641941a06079164f474d61e02c121a4
alt <>
2023-08-12 22:11:57 +0800

    net work

--- src/pages/Network/Mobile.js
+++ src/pages/Network/Mobile.js
@@ -2,59 +2,59 @@ import React, { useState } from "react"
 import "./Mobile.css"
 import MHeader from "../../componentM/Header"
 import MFooter from "../../componentM/Footer"
-import about from "../../static/about.png"
+import pdf from '../../static/pdf.png'
 
 export default function NetworkMobile() {
     const [showIndex, setShowIndex] = useState(0)
 
     const workData = [
         {
-            img: about,
+            img: 'https://static.paraluni.org/images/infiweb/lighting_bg2.png',
             title: "The Lightning Network is dependent upon the underlying technology of the blockchain. By using real Bitcoin/blockchain transactions and using its native smart-contract scripting language, it is possible to create a secure network of participants which are able to transact at high volume and high speed.",
         },
         {
-            img: about,
+            img: 'https://static.paraluni.org/images/infiweb/lighting_bg3.png',
             title: "Bidirectional Payment Channels. Two participants create a ledger entry on the blockchain which requires both participants to sign off on any spending of funds. Both parties create transactions which refund the ledger entry to their individual allocation, but do not broadcast them to the blockchain. They can update their individual allocations for the ledger entry by creating many transactions spending from the current ledger entry output. Only the most recent version is valid, which is enforced by blockchain-parsable smart-contract scripting. This entry can be closed out at any time by either party without any trust or custodianship by broadcasting the most recent version to the blockchain.",
             type: "end",
         },
         {
-            img: about,
+            img: 'https://static.paraluni.org/images/infiweb/lighting_bg4.png',
             title: "Lightning Network. By creating a network of these two-party ledger entries, it is possible to find a path across the network similar to routing packets on the internet. The nodes along the path are not trusted, as the payment is enforced using a script which enforces the atomicity (either the entire payment succeeds or fails) via decrementing time-locks.",
         },
         {
-            img: about,
+            img: 'https://static.paraluni.org/images/infiweb/lighting_bg5.png',
             title: "Blockchain as Arbiter. As a result, it is possible to conduct transactions off-blockchain without limitations. Transactions can be made off-chain with confidence of on-blockchain enforceability. This is similar to how one makes many legal contracts with others, but one does not go to court every time a contract is made. By making the transactions and scripts parsable, the smart-contract can be enforced on-blockchain. Only in the event of non-cooperation is the court involved – but with the blockchain, the result is deterministic.",
             type: "end",
         },
     ]
     const infinitasData = [
         {
-            img: about,
+            img: 'https://static.paraluni.org/images/infiweb/lighting_bg6.png',
             sub: "Infinitas embeds the RGB protocol into the transactions of the Lightning Network channel, establishes a series of two-way payment channels outside the blockchain, and utilizes the efficient performance of the Lightning Network to allow users to conduct fast and low-cost transactions on these channels.",
         },
         {
-            img: about,
+            img: 'https://static.paraluni.org/images/infiweb/lighting_bg7.png',
             sub: "Once the Lightning Network channel is established, users can conduct RGB protocol transactions in the channel. This means that users can create and trade RGB tokens, and these transactions are not directly written to the Bitcoin main blockchain, but are carried out within the channel and do not directly affect the Bitcoin main blockchain.",
         },
         {
-            img: about,
+            img: 'https://static.paraluni.org/images/infiweb/lighting_bg8.png',
             sub: "The more complex smart contracts supported by Infinitas allow users to perform more types of transaction operations within Lightning Network channels, including conditional payments, multi-party signatures, and more. It provides users with more flexible and diversified trading options.",
         },
         {
-            img: about,
+            img: 'https://static.paraluni.org/images/infiweb/lighting_bg9.png',
             sub: "The combination of Infinitas brings more functionality and scalability to the Bitcoin ecosystem, providing users with a better transaction experience and more usage options. At the same time, the transactions in the Lightning Network are carried out in the channel and will not be immediately exposed to the entire Bitcoin network, thus providing higher transaction privacy to a certain extent.",
         },
     ]
 
     const referenceData = [
         {
-            img: about,
+            img: pdf,
             title: "The Bitcoin Lightning Network",
             link: "https://lightning.network/lightning-network-summary.pdf",
             button: "GO",
         },
         {
-            img: about,
+            img: pdf,
             title: "The Bitcoin Lightning Network: Scalable Off-Chain Instant Payments",
             link: "https://lightning.network/lightning-network-paper.pdf",
             button: "GO",
@@ -71,7 +71,7 @@ export default function NetworkMobile() {
                 <div className="network-sub colorB">
                     Lightning is a decentralized network using smart contract functionality in the blockchain to enable instant payments across a network of participants.
                 </div>
-                <img src={about} className="network-header-img" />
+                <img src={'https://static.paraluni.org/images/infiweb/lighting_bg1.png'} className="network-header-img" />
             </div>
             <div className="network-work content">
                 <div className="network-text colorF">HOW IT WORKS</div>
