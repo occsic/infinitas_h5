129c7f5f0b98adfc8fd5253c157e5fed9e77494b
evercurse <17601547148@163.com>
2023-08-17 10:57:49 +0800

    article

---
+++ src/pages/Article/Mobile.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './Mobile.css'
+
+export default function ArticleMobile(){
+    return (
+        <div>
+            111
+        </div>
+    )
+}
---
+++ src/pages/Article/PC.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './PC.CSS'
+
+export default function ArticlePc(){
+    return (
+        <div>
+            111
+        </div>
+    )
+}
